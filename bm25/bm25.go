b0ef7e51d4a35752d9375e76f3a982ca2d773206
wim_spaargaren@live.nl <wim_spaargaren@live.nl>
2017-02-23 20:54:23 +0100

    added some comments to provide clarity in the algorithm

--- bm25/bm25.go
+++ bm25/bm25.go
@@ -7,40 +7,39 @@ import (
 
 //varies from 1.2-2.0
 var k1 float64 = 1.2
+
+//should be around 0.75
 var b float64 = 0.75
 var docs []string
 
 func main() {
-
 	docs = []string{"Shipment of gold damaged in a fire", "Delivery of silver arrived in a silver truck", "Shipment of gold arrived in a truck"}
 	terms := []string{"gold", "silver", "truck"}
 	for i := 0; i < len(docs); i++ {
 		println(docs[i])
-		println(BM25(terms, docs[i], AverageDocLength()))
+		println(BM25(terms, docs[i]))
 	}
 }
 
 //BM25 first param is terms, second is current document
-func BM25(terms []string, d string, L_ave float64) float64 {
-
+func BM25(terms []string, d string) float64 {
 	//total number of docs
 	N := float64(len(docs))
-
-	//search terms
-
 	//document length
 	L_d := float64(len(d))
+	//average documnet length
+	L_ave := AverageDocLength()
 
 	sum := 0.0
 	for j := 0; j < len(terms); j++ {
 		//current term
 		i := terms[j]
-		tf_id := TermFreq(d, i)
+		//left hand side of the multiplication
 		idf := math.Log10(N / NumberOfDocOccurrences(i))
-		// docsWithTerm := NumberOfDocOccurrences(i)
-		// idf := math.Log10(N/docsWithTerm + 0.5)
+		//right hand side of the multiplication
+		tf_id := TermFreq(d, i)
 		rightHandSide := ((k1 + 1) * tf_id) / (k1*((1-b)+b*(L_d/L_ave)) + tf_id)
-
+		//add it to the sum
 		sum += (idf * rightHandSide)
 	}
 	return sum
