d0735fff7f8977872fcf5794e714392846835d64
Rick Proost <rick@weave.nl>
2017-02-23 11:45:02 +0100

    get secrets from running vars

--- streamtwitter.go
+++ streamtwitter.go
@@ -8,26 +8,34 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/coreos/pkg/flagutil"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
 
 func main() {
-	flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
-	consumerKey := flags.String("consumer-key", "", "Twitter Consumer Key")
-	consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
-	accessToken := flags.String("access-token", "", "Twitter Access Token")
-	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
-	flags.Parse(os.Args[1:])
-	flagutil.SetFlagsFromEnv(flags, "TWITTER")
+	opts := struct {
+		TCKey    string
+		TCSecret string
+		TAToken  string
+		TASecret string
+	}{}
+	flag.StringVar(&opts.TCKey, "tckey", opts.TCKey, "input for Twitter consumer key")
+	flag.StringVar(&opts.TCSecret, "tcsecret", opts.TCSecret, "input for Twitter consumer secret")
+	flag.StringVar(&opts.TAToken, "tatoken", opts.TAToken, "input for Twitter access token")
+	flag.StringVar(&opts.TASecret, "tasecret", opts.TASecret, "input for Twitter access secret")
+	flag.Parse()
 
-	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
+	consumerKey := opts.TCKey
+	consumerSecret := opts.TCSecret
+	accessToken := opts.TAToken
+	accessSecret := opts.TASecret
+
+	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
 		log.Fatal("Consumer key/secret and Access token/secret required")
 	}
 
-	config := oauth1.NewConfig(*consumerKey, *consumerSecret)
-	token := oauth1.NewToken(*accessToken, *accessSecret)
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
 	// OAuth1 http.Client will automatically authorize Requests
 	httpClient := config.Client(oauth1.NoContext, token)
 
