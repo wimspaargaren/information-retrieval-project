81b5521dc0dd190a67cc786cd1af08fc3e6088a2
wim_spaargaren@live.nl <wim_spaargaren@live.nl>
2017-03-14 16:50:30 +0100

    tried something for 5000 tweets with running taxonomy

--- src/dataPreProcessing/ProcessData.py
+++ src/dataPreProcessing/ProcessData.py
@@ -11,6 +11,8 @@ import json
 import codecs
 import bm25
 
+globalTweetIdList = []
+
 def getTweets() : 
     print("Retrieving tweets: \n")
     try:
@@ -20,7 +22,7 @@ def getTweets() :
 
     cur = conn.cursor()
     try:
-        cur.execute("""SELECT * from data limit 5""")
+        cur.execute("""SELECT * from data limit 5000""")
     except:
         print "I can't select from specified table!"
 
@@ -30,19 +32,30 @@ def getTweets() :
     for row in rows:
         array = json.dumps(row[1])
         a = json.loads(array)
-        print("Tweet:")
-        print(a["text"].encode("utf-8").rstrip("\r\n"))
-        tweetList.append(a["text"].encode("utf-8").rstrip("\r\n"))
+      #  print("Tweet:")
+        globalTweetIdList.append(rows[0])
+      #  print(a["text"].encode("utf-8").rstrip("\r\n"))
+        tweetList.append(a["text"].encode("utf-8").rstrip("\r\n").lower())
     return tweetList
  
 
 if __name__ == '__main__' :
     tweetList = getTweets()
+    print("Initing BM25\n")
     bm25 = bm25.BM25(tweetList, delimiter=' ')
-    Query = 'kennis land'
+    #Query = 'voetbal voetballen voetbalschoen scheenbeschermers scheenbeschermer arena soccer'
+    Query = 'hardlopen rennen running hard lopen run jogging marathon'
     Query = Query.split()
+    print("Calculating scores\n")
     scores = bm25.BM25Score(Query)
-    print scores
+    counter = 0
+    print("Scores:\n")
+    for score in scores:
+        if score > 0 :
+            print(score)
+            print(tweetList[counter])
+        
+        counter = counter + 1
  #   tfidf = bm25.TFIDF()
   #  print bm25.Items()
  #   for i, tfidfscore in enumerate(tfidf):
