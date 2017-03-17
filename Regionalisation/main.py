392e8d8e6fde20ec0ec5bc442f7a72b53f02c7e8
Thom Hubers <info@thomhubers.nl>
2017-03-17 22:03:04 +0100

    Playing with the tutorial
    Next step is making regions of amsterdam and weigh them with tweets. In
    that way we do neighbourhood classification instead of hotspot/cluster
    analysis.

--- Regionalisation/main.py
+++ Regionalisation/main.py
@@ -1,3 +1,20 @@
 import pysal
+import numpy as np
+import random
 
-help(pysal)
+example_path = "C:/Python27/Lib/site-packages/pysal/examples"
+
+f = pysal.open(example_path + "/us_income/usjoin.csv")
+pci = np.array([f.by_col[str(y)] for y in range(1929, 2010)])
+pci = pci.transpose()
+pci.shape
+
+print(pci.shape)
+
+w = pysal.open(example_path + "/us_income/states48.gal").read()
+
+np.random.seed(100)
+random.seed(10)
+r = pysal.Maxp(w, pci, floor = 5, floor_variable = np.ones((48, 1)), initial = 99)
+
+print(r.regions)
