e0414e4e43ed572cc09c238d4d08213e1342c2a1
rpjproost <rickproost181@hotmail.com>
2017-03-17 10:30:22 +0100

    fixed

--- src/mapBoxAPi/routes.go
+++ src/mapBoxAPi/routes.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
+import "net/http"
 
 //Route route struct containing the name of the rout, the http method, the pattern and the function which handles the route.
 type Route struct {
@@ -18,19 +14,19 @@ type Route struct {
 type Routes []Route
 
 //NewRouter initializes the mux with the defined routes.
-func NewRouter() *mux.Router {
+// func NewRouter() *mux.Router {
 
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(route.HandlerFunc)
-	}
+// 	router := mux.NewRouter().StrictSlash(true)
+// 	for _, route := range routes {
+// 		router.
+// 			Methods(route.Method).
+// 			Path(route.Pattern).
+// 			Name(route.Name).
+// 			Handler(route.HandlerFunc)
+// 	}
 
-	return router
-}
+// 	return router
+// }
 
 var routes = Routes{
 	Route{
--- src/mapBoxAPi/server.go
+++ src/mapBoxAPi/server.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"github.com/unrolled/render"
 )
 
+var rOutput *render.Render
+
 func main() {
-	setupDummyIssues()
-	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// setupDummyIssues()
+	// router := NewRouter()
+	var origins []string
+	origins = append(origins, "*")
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   origins,
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"*"},
+		AllowedMethods:   []string{"POST", "GET", "DELETE", "PUT"},
+	})
+
+	mux := mux.NewRouter()
+
+	// Routes
+	mux.HandleFunc("/index", Index).Methods("GET")
+	mux.HandleFunc("/getpoints", GetPoints).Methods("GET")
+
+	n := negroni.Classic()
+	n.Use(c)
+	n.UseHandler(mux)
+	n.Run(":8080")
+	// log.Fatal(http.ListenAndServe(":8080", router))
 }
