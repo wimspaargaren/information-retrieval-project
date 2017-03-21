e8fdd253debda8685324be2e88a6e0b82fa28a36
rpjproost <rickproost181@hotmail.com>
2017-03-21 14:57:41 +0100

    filter on day working

--- src/mapBoxAPi/js/filter.js
+++ src/mapBoxAPi/js/filter.js
@@ -1,54 +1,55 @@
 
+
+$( document ).ready(function() {
+    $('#dayfilter').on('change', function(event) {
+        filterOnDay(parseInt(event.target.value));
+    })
+});
+
 function filterOnDay(number) {
+    if(number == 0) {
+        setDefaultLayer();
+        return;
+    }
     var day = getDayFromNumber(number);
     map.removeSource("point")
 
-    if(map.getLayer(day) !== undefined) {
-        var visibility = map.getLayoutProperty(day, 'visibility');
-
-        if (visibility === 'visible') {
-            map.setLayoutProperty(day, 'visibility', 'none');
-            this.className = '';
-        } else {
-            this.className = 'active';
-            map.setLayoutProperty(day, 'visibility', 'visible');
+    var resJson = {features: [], type: "FeatureCollection"};
+    for(var i=0; i<geojson.features.length; i++) {
+        if(geojson.features[i].properties.day == day) {
+            resJson.features.push(geojson.features[i]);
         }
-    } else {
-        var resJson = {features: [], type: "FeatureCollection"};
-        for(var i=0; i<geojson.features.length; i++) {
-            if(geojson.features[i].properties.day == day) {
-                resJson.features.push(geojson.features[i]);
-            }
-        }
-
-        map.addSource('point', {
-            "type": "geojson",
-            "data": resJson
-        });
-
-        map.addLayer({
-            "id": day,
-            "type": "circle",
-            "source": "point",
-            'paint': {
-                // make circles larger as the user zooms from z12 to z22
-                'circle-radius': {
-                    'base': 5,
-                    'stops': [[12, 6], [22, 180]]
-                },
-                // color circles by ethnicity, using data-driven styles
-                'circle-color': {
-                    property: 'sport-category',
-                    type: 'categorical',
-                    stops: [
-                        ['Voetbal', 'red'],
-                        ['Tennis', 'green'],
-                        ['hardlopen', 'blue']
-                    ]
-                }
-            }
-        });
     }
+
+    map.addSource('point', {
+        "type": "geojson",
+        "data": resJson
+    });
+
+    map.addLayer({
+        "id": day,
+        "type": "circle",
+        "source": "point",
+        'paint': {
+            // make circles larger as the user zooms from z12 to z22
+            'circle-radius': {
+                'base': 5,
+                'stops': [[12, 6], [22, 180]]
+            },
+            // color circles by ethnicity, using data-driven styles
+            'circle-color': {
+                property: 'sport-category',
+                type: 'categorical',
+                stops: [
+                    ['Voetbal', 'red'],
+                    ['Tennis', 'green'],
+                    ['hardlopen', 'blue']
+                ]
+            }
+        }
+    });
+    console.log("Filter on " + day);
+    
 }
 
 function getDayFromNumber(number) {
@@ -62,4 +63,36 @@ function getDayFromNumber(number) {
         case 7 : return "Sunday";
         default: return "Monday";
     }
+}
+
+function setDefaultLayer() {
+    map.removeSource("point")
+
+    map.addSource('point', {
+        "type": "geojson",
+        "data": geojson
+    });
+
+    map.addLayer({
+        "id": "point",
+        "type": "circle",
+        "source": "point",
+        'paint': {
+            // make circles larger as the user zooms from z12 to z22
+            'circle-radius': {
+                'base': 5,
+                'stops': [[12, 6], [22, 180]]
+            },
+            // color circles by ethnicity, using data-driven styles
+            'circle-color': {
+                property: 'sport-category',
+                type: 'categorical',
+                stops: [
+                    ['Voetbal', 'red'],
+                    ['Tennis', 'green'],
+                    ['hardlopen', 'blue']
+                ]
+            }
+        }
+    });
 }
