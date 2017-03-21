4202a9a72a5ef5c98af1da5ad3c2bfd67cdb9c8b
wim_spaargaren@live.nl <wim_spaargaren@live.nl>
2017-03-21 17:20:44 +0100

    added querys in ProcessData.py and corrected README

--- src/dataPreProcessing/ProcessData.py
+++ src/dataPreProcessing/ProcessData.py
@@ -37,7 +37,6 @@ def getTweets() :
 def updateTweets(category, idUpdateSTring) :
     connString = "dbname='"+sys.argv[1]+"' user='"+sys.argv[2]+"' host='"+sys.argv[3]+"' password='"+sys.argv[4]+"' port='"+sys.argv[5]+"'"
     updateQuery = "UPDATE data SET category = '"+ category +"' WHERE id IN ("+idUpdateSTring+")"
-    print("""SELECT * from data limit 5000""")
     print(updateQuery)
     try:
         conn = psycopg2.connect(connString)
@@ -58,10 +57,39 @@ if __name__ == '__main__' :
     print("Initing BM25\n")
     bm25 = bm25.BM25(tweetList, delimiter=' ')
     #Seperate list of words with comma remove spaces!
-    #Query = 'voetbal,voetballen,voetbalschoen,scheenbeschermers,scheenbeschermer,arena,soccer'
-    category = "hardlopen"
-    Query = 'hardlopen,rennen,running,lopen,run,jogging,marathon,gerend,gelopen,hardgelopen,ran,hard lopen'
-    #Query = 'tennis,tennisracket,racket,tennisbaan,tennisbal-ball,tennisball,tennis court'
+    
+    #category = "fitness"
+    #Query = 'fitness,steps,spinning,aerobics,squat,deadlift,bench press,military press, pull up,gym,sportschool,home,thuis,indoorroeier,crosstrainer,hometrainer,calf raises,chin-up,opdrukken,dumbell flexion extension,lunge,bankdrukken,grapevine,zumba,bodystep'
+    
+    #category = "soccer"
+    #Query = 'voetbal,football,soccer,speeldveld,voetbaleveld,voetbalterrein,grasveld,bal,ball,voetbalschoenen,voetbalshirt,voetbalbroek,voetbalschoenen,scheenbeschermers,scheidsrechter,referee,doel,doelpunt,doel,blessuretijd,strafschoppen'
+
+    #category = "running"
+    #Query = 'hardlopen,rennen,running,lopen,run,jogging,marathon,gerend,gelopen,hardgelopen,ran,hard lopen,duursport,hardloopschoenen,hardloopkleding'
+  
+    #category = "swimming"
+    #Query = 'swimming,zwemmen,water,pools,zwembad,zwemmend,gezwommen,vrije slag,vlinder slag,rugslag,schoolslag,borstcrawl,rugcrawl,wisselslag,wedstrijdzwemmen,openwaterzwemmen,schoonspringen,synchroonzwemmen,vinzwemmen,waterbasketbal,waterpolo,winterzwemmen,zwemmend redden,elementair zwemmen'
+
+    #category = "fightingsport"
+    #Query = 'vechtsport,fighting sport,vechtensporten,boksen,kung fu,worstelen,schermen,karate,MMA,kickboksen,zelfverdediging,judo'
+
+    #category = "cycling"
+    #Query = 'wielrennen,wielersport,fietsen,racefiets,wielrenfiets,cycling,fiets,bicycle,baanwielrennen,bmx,mountainbiken,wegwielrennen,wielrennersshirt,wielrennersbroek,wielrennersschoenen,helm'
+
+    #dancing query must be updated.
+
+    #category = "gymnastics"
+    #Query = 'Gymnastics,athletics,exercises,aerobics,aerobic,acrobatics,bars,rings,turnen,leotard,turnace,turnace hall,gym,gymen'
+
+   # category = "hockey"
+   # Query = 'hockey,hockey stick,hockey veld,Jockstrap,protective cup,cup,puck,unicycle hockey'
+
+    #category = "yoga"
+    #Query = 'Yoga,Indische mystiek,meditatie,mediteren,meditation,meditate,concentration,concentreren,bewustzijn,mindfulness,Ascetische leer,Bewegingsleer,Bewustzijnsoefening,dojo,flexibility,stamina,stress,anti-stress,nutrition,Fitstar,daily yoga,down dog'
+
+    category = "bootcamp"
+    Query = 'Bootcamp,crossfit,insanity,tabata,circuittraining,move,pull,push,core,intensief,intensive,grenzen verleggen,Core Power,FitDeck,FitMoves,exercises'
+
     Query = Query.split(",")
     for query in Query :
         print query
