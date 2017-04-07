c5c2175f4979be0fd36bca3dfb7ee5cce8880f22
Wim Spaargaren <wim_spaargaren@live.nl>
2017-04-07 10:08:54 +0200

    added dancing category to preprocess

--- src/dataPreProcessing/ProcessData.py
+++ src/dataPreProcessing/ProcessData.py
@@ -40,7 +40,7 @@ def getTweets() :
     return tweetList
     
 class sportTweet():
-    def __init__(self,tweetID,category,bm_running,bm_gymnastics,bm_cycling,bm_bootcamp,bm_fightingsport,bm_yoga,bm_soccer,bm_fitness,bm_swimming):
+    def __init__(self,tweetID,category,bm_running,bm_gymnastics,bm_cycling,bm_bootcamp,bm_fightingsport,bm_yoga,bm_soccer,bm_fitness,bm_swimming,bm_dancing):
         print(tweetID)
         self.category = category
         self.bm_running = bm_running
@@ -52,6 +52,7 @@ class sportTweet():
         self.bm_soccer = bm_soccer
         self.bm_fitness = bm_fitness
         self.bm_swimming = bm_swimming
+        self.bm_dancing = bm_dancing
         self.tweetID = tweetID
 
 def getSportTweets() : 
@@ -64,7 +65,7 @@ def getSportTweets() :
 
     cur = conn.cursor()
     try:
-        cur.execute("""SELECT id, category, bm_running,bm_gymnastics, bm_cycling, bm_bootcamp, bm_fightingsport, bm_yoga,bm_soccer, bm_fitness, bm_swimming FROM data WHERE bm_running IS NOT NULL OR bm_gymnastics IS NOT NULL OR bm_cycling IS NOT NULL OR bm_bootcamp IS NOT NULL OR bm_fightingsport IS NOT NULL OR bm_yoga IS NOT NULL OR bm_soccer IS NOT NULL OR bm_fitness IS NOT NULL OR bm_swimming IS NOT NULL;""")
+        cur.execute("""SELECT id, category, bm_running,bm_gymnastics, bm_cycling, bm_bootcamp, bm_fightingsport, bm_yoga,bm_soccer, bm_fitness, bm_swimming, bm_dancing FROM data WHERE bm_running IS NOT NULL OR bm_gymnastics IS NOT NULL OR bm_cycling IS NOT NULL OR bm_bootcamp IS NOT NULL OR bm_fightingsport IS NOT NULL OR bm_yoga IS NOT NULL OR bm_soccer IS NOT NULL OR bm_fitness IS NOT NULL OR bm_swimming IS NOT NULL;""")
     except:
         print "I can't select from specified table!"
 
@@ -72,9 +73,13 @@ def getSportTweets() :
 
     tweetList = []
     for row in rows:
-        tweetList.append(sportTweet(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
+        tweetList.append(sportTweet(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11]))
 
     return tweetList
+
+def resetCatgories() : 
+    print("TODO RESET CATEGORIES")
+
 
 def updateBmColumns(updateQuerys):
     connString = "dbname='"+sys.argv[1]+"' user='"+sys.argv[2]+"' host='"+sys.argv[3]+"' password='"+sys.argv[4]+"' port='"+sys.argv[5]+"'"
@@ -107,7 +112,7 @@ if __name__ == '__main__' :
     queryList.append(Query("swimming",'swimming,zwemmen,water,pools,zwembad,zwemmend,gezwommen,vrije slag,vlinder slag,rugslag,schoolslag,borstcrawl,rugcrawl,wisselslag,wedstrijdzwemmen,openwaterzwemmen,schoonspringen,synchroonzwemmen,vinzwemmen,waterbasketbal,waterpolo,winterzwemmen,zwemmend redden,elementair zwemmen'))
     queryList.append(Query("fightingsport",'vechtsport,fighting sport,vechtensporten,boksen,kung fu,worstelen,schermen,karate,MMA,kickboksen,zelfverdediging,judo'))
     queryList.append(Query("cycling",'wielrennen,wielersport,fietsen,racefiets,wielrenfiets,cycling,fiets,bicycle,baanwielrennen,bmx,mountainbiken,wegwielrennen,wielrennersshirt,wielrennersbroek,wielrennersschoenen,helm'))
-    #dancing query must be updated.
+    queryList.append(Query("dancing", 'Academisch ballet,Academische dans,Acrobatische rock,Aerobics,American Rhythm,American Smooth,Argentijnse tango,Bachata,Balboa,Balfolk,Ballet,Ballroom,Ballroomtango,Basse danse,Batucada,Biodanza,Bolero,Boogiewoogie,Bostella,Bourree,Breakdance,Buikdans,Cachucha,Cakewalk,Calypso,Cancan,Capoeira,Chachacha,Charleston,Cheerleading,Chunga,Coco,Conga,Congada,Cumbia,Computerdans,Dancehall,Dansexpressie,Danza,Discofox,Dodendans,Dubstep,Engelse wals,Euritmie,Fandango,Frevo,Freestyle,Flamenco,Foxtrot,Gaillarde,Gavotte,Gigue,Hakken,Headbangen,Hiphop,Hora,Hula,Jazzballet,Jive,Jumpstyle,Karakterdans,Kathakali,Klassiek ballet,Klompendans,Krumping,Lambada,Landler,Limbo,Lindy hop,Linedance,Locking,Macarena,Mambo,Mazurka,Melbourne Shuffle,Menuet,Merengue,Moonwalk,Moshen,Moderne dans,Moresca,Paaldansen,Paso doble,Pogo,Polka,Polonaise,Popping,Quadrille,Quickstep,Ragga,Reggaeton,Reidans,Repetitieve dans,Riverdance,Rondedans,Rueda de Casino,Rumba,Salsa,Saltarello,Samba,Sarabande,Showdance,Sirtaki,Skanken,Slowfox,Son,Stijldansen,Streetdance,Striptease,Swing,Swingfox,Tango,Tapdansen,Tecktonik,Twirling,Twist,Volksdans,Vogue,Vogeltjesdans,Wals,Weense wals,Ballet,Concert dance,Contact improvisation,Contemporary dance,Ecstatic dance,Jazz dance,Lyrical dance,Postmodern dance,Performance dance,Tap dance,American Rhythm,Bolero,East Coast Swing,Mambo,Rumba,Cha Cha,Cumbia,Argentine tango,Jive,Merengue,Milonga,Reggaeton,Rueda,Salsa,Samba,ballroom dance,Samba,Brazilian dance,Samba de Gafieira,Zumba,dance,dansen,dans,Zouk,Quebradita,gayunder,Balboa,Blues dance,Carolina Shag,Charleston,Collegiate Shag,East Coast Swing,Hand Jive,Jitterbug,Jive,Leroc,Lindy Hop,Modern Jive,St. Louis Shag,West Coast Swing,Traditional jazz,Foxtrot,Tango,Viennese Waltz,Waltz,Quickstep,Bharatanatyam,Chhau dance,Gaudiya Nritya,Kathak,Kathakali,Kuchipudi,Manipuri dance,Mohiniyattam,Odissi,Sattriya,Crowdsurfing,Moshing,Skanking,Jump up,Footwork,Powermoves,Toprock,Bounce,Clown Walk,Electric boogaloo,Flexing,Floating,Krumping,Litefeet,Robot dance,hip hop,Memphis Jookin,Animation,Hit Dem Folks,Liquiding,Moonwalk,Scarecrow,Strobing,Waving,Snap dance,Turfing,Disco dance,Electro Dance,Free step,Grinding,Hard dance,Hardcore dancing,House dance,Hustle,Jacking,Jumpstyle,Melbourne Shuffle,Slothing,Vogue,Waacking,Hard Rock Pogo,Metal Mosh,Punk Rock Pogo,Ska Dance,Baroque dance,English country dance,Masque,Medieval dance,Regency dance,Vintage dance,Liturgical dance,Acro dance,Belly dance,Bollywood dance,Calypso,Fire dance,Flamenco,Flying Men Dance,Hip Hop,Kizomba,Line dance,Native American,Novelty and fad dances,Reggae,Salsaton,Semba,Soca,hooping,Zumba,The Routledge Dance Studies Reader,The dance; an historical survey of dancing in Europe,The Body, Dance and Cultural Theory,Modern Tap Dance,Jazz,Rhythm,Body and Soul'))
     queryList.append(Query("gymnastics",'Gymnastics,athletics,exercises,aerobics,aerobic,acrobatics,bars,rings,turnen,leotard,turnace,turnace hall,gym,gymen'))
     queryList.append(Query("hockey",'hockey,hockey stick,hockey veld,Jockstrap,protective cup,cup,puck,unicycle hockey'))
     queryList.append(Query("yoga",'Yoga,Indische mystiek,meditatie,mediteren,meditation,meditate,concentration,concentreren,bewustzijn,mindfulness,Ascetische leer,Bewegingsleer,Bewustzijnsoefening,dojo,flexibility,stamina,stress,anti-stress,nutrition,Fitstar,daily yoga,down dog'))
@@ -130,9 +135,9 @@ if __name__ == '__main__' :
         print("Updating bm columns")
         updateBmColumns(updateQueryList) 
 
-    print("@TODO Update tweet categories")
+    resetCatgories()
     updateSportCategories = []
-    sporTweets = getSportTweets();
+    sporTweets = getSportTweets()   
     for t in sporTweets:
         maxval = 0.0
         category = "undefined"
@@ -163,6 +168,9 @@ if __name__ == '__main__' :
         if t.bm_swimming > maxval :
             maxval = t.bm_swimming
             category = "swimming"
+        if t.bm_dancing > maxval :
+            maxval = t.bm_dancing
+            category = "dancing"
         print(category)
         updateSportCategories.append("UPDATE data SET category = '"+ category +"' WHERE id = " + str(t.tweetID))
         
