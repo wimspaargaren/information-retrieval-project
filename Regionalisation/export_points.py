e37570a694b04779f099d343913fbc02460065dd
Thom Hubers <info@thomhubers.nl>
2017-04-12 22:04:25 +0200

    strava and lots of things

--- Regionalisation/export_points.py
+++ Regionalisation/export_points.py
@@ -1,7 +1,9 @@
-# Can be imported in QGIS
+# Export points
+# Can be imported in QGIS in order to do Voronoi there
 
 from classes import database
 import numpy as np
+import time
 
 conn = database.connect()
 
@@ -12,7 +14,25 @@ tweets = db.fetchall()
 tweets2 = []
 
 for tweet in tweets:
-    tweets2.append((tweet[0], tweet[5], tweet[6], tweet[8], tweet[9], tweet[10], tweet[11],
-                    tweet[12], tweet[13], tweet[14], tweet[15], tweet[16]))
+    replaced = [0 if v is None else v for v in tweet[8:]]
 
-np.savetxt("E:/information_retrieval/tweets.csv", tweets2, delimiter=";", fmt='%s')
+    # Check if location already exists
+    overlap = [t for t in tweets2 if t[1] == tweet[5] and t[2] == tweet[6]]
+
+    if len(overlap) > 0:
+        # Get existing overlapping item
+        for idx, item in enumerate(tweets2):
+            if tweet[5] == item[1] and tweet[6] == item[2]:
+                # Add values to existing item
+                item[3:] = [sum(x) for x in zip(item[3:], replaced)]
+
+                # Put item back
+                tweets2[idx] = item
+    else:
+        # Get ID and position
+        reformat = [tweet[0], tweet[5], tweet[6]]
+        # Add values
+        reformat.extend(replaced)
+        tweets2.append(reformat)
+
+np.savetxt("E:/information_retrieval/tweets" + str(time.time()) + ".csv", tweets2, delimiter=";", fmt='%s')
--- Regionalisation/main.py
+++ Regionalisation/main.py
@@ -8,6 +8,7 @@ import sys
 import settings as s
 import time
 import mplleaflet
+from numpy import genfromtxt
 
 
 def connect():
@@ -22,16 +23,22 @@ def connect():
     return conn
 
 # config
-write_database = False
-write_screen = True
+write_database = True
+write_screen = False
 write_map = True
+strava = True
 plt.hold(True)
 
 # get stuff
 conn = connect()
 db = conn.cursor()
-db.execute("SELECT * FROM public.data WHERE category IS NOT NULL ORDER BY id")
-tweets = db.fetchall()
+# db.execute("SELECT * FROM public.data WHERE category IS NOT NULL ORDER BY id")
+# tweets = db.fetchall()
+
+if strava:
+    tweets = genfromtxt('E:/information_retrieval/strava1492007566.76.csv', delimiter=';')
+else:
+    tweets = genfromtxt('E:/information_retrieval/tweets1491997871.83.csv', delimiter=';')
 
 # Create a set
 cluster_config_id = -1
@@ -47,11 +54,14 @@ if write_screen:
     print "Cluster " + str(cluster_set_id) + " created at " + str(time.time())
 
 # Filter categories
-categories = []
-for tweet in tweets:
-    if tweet[7] not in categories:
-        categories.append(tweet[7])
-print categories
+if strava:
+    pass
+else:
+    categories = []
+    for tweet in tweets:
+        if tweet[7] not in categories:
+            categories.append(tweet[7])
+    print categories
 
 # construct lists
 id_list = []
@@ -59,75 +69,126 @@ position_list = []
 categories = []
 skipped = []
 
-
-for tweet in tweets:
-    location = (tweet[5], tweet[6])
-
-    # There is an issue with identical coordinates, we filter those out now and keep track of them
-    # We can add them to the clusters later
-    if location not in position_list:
-        id_list.append(tweet[0])  # ID
-        position_list.append(location)  # LAT, LON
-
-        category_weights = list(tweet[8:])
-        replaced = [0 if v is None else v for v in category_weights]
-        categories.append(replaced)
-
-    else:
-        identical_index = position_list.index(location)
-        skipped.append((tweet[0], id_list[identical_index]))  # Tup(ID, ID of identical)
-
+# for tweet in tweets:
+#     location = (tweet[5], tweet[6])
+#
+#     id_list.append(tweet[0])  # ID
+#     position_list.append(location)  # LAT, LON
+#
+#     category_weights = list(tweet[8:])
+#     replaced = [0 if v is None else v for v in category_weights]    # Null -> 0
+#     categories.append(replaced)
 
 # perform kNN
-for amount_neighbours in [5]:
-    # kd = pysal.cg.kdtree.KDTree(np.array(position_list))
-    # w = pysal.weights.Distance.KNN(kd, amount_neighbours)
-    w = pysal.weights.Contiguity.Rook.from_shapefile("E:/information_retrieval/voronoi.shp", "field_1")
 
-    z = np.random.random_sample((w.n, 2))
+# kd = pysal.cg.kdtree.KDTree(np.array(position_list))
+# w = pysal.weights.Distance.KNN(kd, amount_neighbours)
+if strava:
+    w = pysal.weights.Contiguity.Rook.from_shapefile("E:/information_retrieval/strava/strava.shp", "field_1")
+else:
+    w = pysal.weights.Contiguity.Rook.from_shapefile("E:/information_retrieval/v3/shapefile.shp", "field_1")
 
-    # z = np.array(categories)
-    # z.transpose()
-    # print z
+# z = np.random.random_sample((w.n, 2))
 
-    p = np.ones((w.n, 1), float)
-    floor = 3
-    solution = pysal.region.Maxp(w, z, floor, floor_variable=p, initial=100)
+# Get attributes for each item in W, put those in the categories list
+if strava:
+    for i in w:
+        from_csv = [x for x in tweets if x[0] == i[0]]     # get categories for this tweet
+        factors = from_csv[0][3:4]
+        categories.append(factors)
+else:
+    for i in w:
+        from_csv = [x for x in tweets if x[0] == i[0]]     # get categories for this tweet
+        bm25s = from_csv[0][3:12]
+        categories.append(bm25s)
 
-    # Set this high (9999) in final product, low in debug mode
-    solution.inference(nperm=9999)
-    print solution.pvalue
+# for index, item in enumerate(w):
+#     print index
+#     print item
+#     print categories[index]
 
-    print str(len(solution.regions)) + " clusters identified "
+z = np.array(categories)
+z.transpose()   # prepare for maxp
+# print z
 
-    print solution.regions
+p = np.ones((w.n, 1), float)
+floor = 3
+solution = pysal.region.Maxp(w, z, floor, floor_variable=p, initial=100)
 
-    for region in solution.regions:
-        data_ids = []
-        data_lats = []
-        data_lons = []
+# Set this high (9999) in final product, low in debug mode
+solution.inference(nperm=9999)
+print solution.pvalue
 
-        for index in region:
-            tweet_id = id_list[index]
-            data_ids.append(tweet_id)
-            data_lats.append(position_list[index][0])
-            data_lons.append(position_list[index][1])
+print str(len(solution.regions)) + " clusters identified "
 
-            # Find skipped ones with this ID and also append them
-            for item in skipped:
-                if item[1] == tweet_id:
-                    data_ids.append(item[0])
+print solution.regions
 
-        if write_database:
-            # Insert regions, possible to add properties
-            query = "INSERT INTO public.clusters(cluster_set_id, data_ids) VALUES (%s, %s);"
-            db.execute(query, (cluster_set_id, data_ids))
+for region in solution.regions:
+    data_ids = []
+    data_lats = []
+    data_lons = []
+    data = []
 
-        if write_screen:
-            print "ID's: " + str(data_ids)
+    # print region
 
-        if write_map:
-            plt.plot(data_lons, data_lats, 'b')  # Draw blue line
+    for tweet_id in region:
+        data_ids.append(tweet_id)
+        from_csv = [x for x in tweets if x[0] == tweet_id][0]  # get tweet
+
+        data_lats.append(from_csv[1])
+        data_lons.append(from_csv[2])
+        data.append(from_csv[3:])
+
+
+        # Find skipped ones with this ID and also append them
+        # for item in skipped:
+        #     if item[1] == tweet_id:
+        #         data_ids.append(item[0])
+
+    # Determine region category
+    if strava:
+        cat_sum = [0, 0]
+        for i in range(2):
+            # aggregate
+            for d in data:
+                cat_sum[i] = cat_sum[i] + d[i]
+    else:
+        cat_sum = [0] * 11
+
+        for i in range(11):
+            # aggregate
+            for d in data:
+                cat_sum[i] = cat_sum[i] + d[i]
+
+    highest_index = 999
+    highest_value = 0
+    for index, item in enumerate(cat_sum):
+        if item > highest_value:
+            highest_value = item
+            highest_index = index
+
+    print highest_index
+
+    if strava:
+        categories = ["running", "cycling"]
+    else:
+        categories = ["running", "gymnastics", "cycling", "bootcamp", "fightingsport",
+                      "yoga", "soccer", "fitness", "swimming", "dancing", "hockey"]
+
+    category = categories[highest_index]
+    print category
+    print highest_value
+
+    if write_database:
+        # Insert regions, possible to add properties
+        query = "INSERT INTO public.clusters(cluster_set_id, data_ids, category) VALUES (%s, %s, %s);"
+        db.execute(query, (cluster_set_id, data_ids, category))
+
+    if write_screen:
+        print "ID's: " + str(data_ids)
+
+    if write_map:
+        plt.plot(data_lons, data_lats, 'b')  # Draw blue line
 
 if write_database:
     conn.commit()
